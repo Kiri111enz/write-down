48a7b99f93118bee9a14449a5e86694a3d14c164
Kiri111enz <omelyanenkokirill@gmail.com>
2023-03-25 15:07:00 +0300

    init postgres

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ dotenv.config();
 
 import path from 'path';
 import express from 'express';
+import sequelize from './db';
 
 const app = express();
 app.use(express.json());
@@ -10,6 +11,8 @@ app.use(express.static(path.resolve(__dirname, '..', '..', 'public')));
 
 const launchServer = async(): Promise<void> => {
     try {
+        await sequelize.authenticate();
+        await sequelize.sync();
         const port = process.env.PORT || 3000;
         app.listen(port, () => console.log(`Server has just started on port ${port}.`));
     }
